28569c16921603dc818bc3e53a9ad41fb916e22f
Vlad <xstep759@gmail.com>
2024-10-30 01:19:06 +0900

    ))

--- src/components/MapPage/index.jsx
+++ src/components/MapPage/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { load } from '@2gis/mapgl';
 
 export default function MapPage() {
+
+//   load().then((mapglAPI) => {
+//     const map = new mapglAPI.Map('foo', {
+//         center: [55.31878, 25.23584],
+//         zoom: 13,
+//         key: '9f817a89-0b26-4d25-8b60-49bee22dee98',
+//     });
+// });
+
+// const marker = new mapgl.Marker(map, {
+//   coordinates: [55.31878, 25.23584],
+// });
+
+
   return (
     <div>
       <h1>Маршрут с 2ГИС API</h1>
-      <MapComponent />
+      <div className="foo w-[500px] h-[300px]"></div>
     </div>
   );
 }
